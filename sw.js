3ef83accc7804b1d99e59fbfc4aede3c1c3ba68d
sylberos17-prog <sylberos17@gmail.com>
2026-09-08 18:05:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "assistant-448-v1";
+const CACHE = "assistant-448-v2";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
